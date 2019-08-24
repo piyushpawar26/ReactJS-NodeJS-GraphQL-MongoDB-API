230adeca838276780745a54ccf5b10416e1df12a
Pawar <pawar@Pawars-MacBook-Air.local>
2019-08-24 18:50:32 +0530

    Added content in modals

--- client/src/components/AddTask.js
+++ client/src/components/AddTask.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Modal } from 'react-materialize';
+import { Modal, Button } from 'react-materialize';
 
-const trigger = <li><NavLink to="/" className="nav-link">Add Task</NavLink></li>;
+class AddTask extends Component {
 
-const AddTask = () => {
-    return (
-        <Modal header="Add Task" trigger={ trigger }>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </Modal>
-    );
+    state = {
+        name: "",
+        description: "",
+        adding: false,
+        open: false
+    }
+
+    onChange = (e) => {
+        this.setState({
+            ...this.state,
+            [e.target.id]: e.target.value
+        });
+    }
+
+    onClick = (e) => {
+        e.preventDefault();
+        this.setState({
+            ...this.state,
+            adding: true,
+            open: true
+        });
+        setTimeout(() => {
+            this.setState({
+                ...this.state,
+                adding: false,
+                open: false
+            });
+            console.log(this.state);
+        }, 4000);
+    }
+
+    render() {
+        const trigger = <li href="#modal-add-task" className="modal-trigger"><NavLink to="/" className="nav-link">Add Task</NavLink></li>;
+
+        const actions = <div>
+            <Button onClick={ (e) => this.onClick(e) } disabled={ this.state.adding }>
+                { !this.state.adding && <span>Add</span> }
+                { this.state.adding && <span>Adding...</span> }
+            </Button>
+            <Button modal="close">Close</Button>
+        </div>;
+
+        const options = {
+            dismissible: false
+        };
+
+        return (
+            <Modal id="modal-add-task" header="Add Task" actions={ actions } open={ this.state.open } trigger={ trigger } options={ options }>
+                <form>
+                    <label>Name:</label>
+                    <input type="text" id="name" value={ this.state.name } onChange={ this.onChange } />
+                    <label>Description:</label>
+                    <input type="text" id="description" value={ this.state.skillset } onChange={ this.onChange } />
+                </form>
+            </Modal>
+        );
+    }
+
 }
 
 export default AddTask;
--- client/src/components/AddWorker.js
+++ client/src/components/AddWorker.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Modal } from 'react-materialize';
+import { Modal, Button } from 'react-materialize';
 
-const trigger = <li><NavLink to="/" className="nav-link">Add Worker</NavLink></li>;
+class AddWorker extends Component {
 
-const AddWorker = () => {
-    return (
-        <Modal header="Add Worker" trigger={trigger}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </Modal>
-    );
+    state = {
+        name: "",
+        skillset: "",
+        adding: false,
+        open: false
+    }
+
+    onChange = (e) => {
+        this.setState({
+            ...this.state,
+            [e.target.id]: e.target.value
+        });
+    }
+
+    onClick = (e) => {
+        e.preventDefault();
+        this.setState({
+            ...this.state,
+            adding: true,
+            open: true
+        });
+        setTimeout(() => {
+            this.setState({
+                ...this.state,
+                adding: false,
+                open: false
+            });
+            console.log(this.state);
+        }, 4000);
+    }
+
+    render() {
+        const trigger = <li href="#modal-add-worker" className="modal-trigger"><NavLink to="/" className="nav-link">Add Worker</NavLink></li>;
+
+        const actions = <div>
+            <Button onClick={ (e) => this.onClick(e) } disabled={ this.state.adding }>
+                { !this.state.adding && <span>Add</span> }
+                { this.state.adding && <span>Adding...</span> }
+            </Button>
+            <Button modal="close">Close</Button>
+        </div>;
+
+        const options = {
+            dismissible: false
+        };
+
+        return (
+            <Modal id="modal-add-worker" header="Add Worker" actions={ actions } open={ this.state.open } trigger={ trigger } options={ options }>
+                <form>
+                    <label>Name:</label>
+                    <input type="text" id="name" value={ this.state.name } onChange={ this.onChange } />
+                    <label>Skillset: (Fill in skills seperated by a ',')</label>
+                    <input type="text" id="skillset" value={ this.state.skillset } onChange={ this.onChange } />
+                </form>
+            </Modal>
+        );
+    }
+
 }
 
 export default AddWorker;
