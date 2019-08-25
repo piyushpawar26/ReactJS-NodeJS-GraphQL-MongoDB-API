d6c39c328b9f8217b2a0d6c7198106a5bab1869f
Pawar <pawar@Pawars-MacBook-Air.local>
2019-08-25 21:24:14 +0530

    Added side navigation and fixed navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import AddWorker from './AddWorker';
 import AddTask from './AddTask';
 import AddAsset from './AddAsset';
+import M from 'materialize-css';
 
-const Navbar = (props) => {
-    return (
-        <div className="navbar-fixed">
-            <nav className="nav-wrapper white z-depth-1">
-                <div className="container">
-                    <Link to="/" className="brand-logo black-text">API</Link>
-                    <ul className="right">
-                        <AddWorker />
-                        <AddTask />
-                        <AddAsset />
+class Navbar extends Component {
+
+    componentDidMount() {
+        var elems = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(elems);
+    }
+
+    render() {
+        return (
+            <div className="navbar-fixed">
+                <nav className="nav-wrapper white z-depth-1">
+                    <div className="container">
+                        <Link to="/" className="brand-logo black-text">API</Link>
+                        <NavLink to="/" data-target="side-navigation" className="sidenav-trigger"><i className="material-icons">menu</i></NavLink>
+                        <ul className="right hide-on-med-and-down">
+                            <AddWorker />
+                            <AddTask />
+                            <AddAsset />
+                        </ul>
+                    </div>
+                </nav>
+                <div className="sidenav" id="side-navigation">
+                    <h5 className="sidenav-logo">API</h5>
+                    <hr />
+                    <ul>
+                        <li href="#modal-add-worker" className="modal-trigger sidenav-close"><Link to="/">Add Worker</Link></li>
+                        <li href="#modal-add-task" className="modal-trigger sidenav-close"><Link to="/">Add Task</Link></li>
+                        <li href="#modal-add-asset" className="modal-trigger sidenav-close"><Link to="/">Add Asset</Link></li>
                     </ul>
                 </div>
-            </nav>
-        </div>
-    );
+            </div>
+        );
+    }
+
 }
 
 export default Navbar;
